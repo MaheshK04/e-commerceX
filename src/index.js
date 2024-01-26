2b08e9eaf8dc7809a93bd40e3375a93787abad4f
MaheshK04 <maheshk04x@gmail.com>
2024-01-26 16:10:04 +0530

    changed version of react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
+import ReactDOM from "react-dom";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container); // Create a root.
-root.render(<App />);
+ReactDOM.render(<App />, container);
